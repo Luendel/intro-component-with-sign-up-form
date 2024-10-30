8f95ce8e21cbfbd557f25faafc37b96eefac31a6
Luendel <luendel3@hotmail.com>
2024-10-30 01:03:04 -0300

    modified:   src/app/app.component.css new file:   src/app/components/form/form.component.css modified:   src/app/components/form/form.component.html modified:   src/app/components/form/form.component.ts new file:   src/app/components/intro/intro.component.css modified:   src/app/components/intro/intro.component.ts new file:   src/app/components/try-free-button/try-free-button.component.css modified:   src/app/components/try-free-button/try-free-button.component.ts modified:   src/index.html

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-intro",
-    templateUrl:"./intro.component.html"
+    templateUrl:"./intro.component.html",
+    styleUrl:"./intro.component.css"
 })
 export class IntroComponent {
 
--- src/app/components/try-free-button/try-free-button.component.ts
+++ src/app/components/try-free-button/try-free-button.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-try-free-button",
-    templateUrl:"./try-free-button.component.html"
+    templateUrl:"./try-free-button.component.html",
+    styleUrl:"./try-free-button.component.css"
 })
 export class TryFreeButton {
 
